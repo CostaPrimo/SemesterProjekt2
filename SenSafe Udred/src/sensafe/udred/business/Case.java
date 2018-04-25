ca1b704a7a7c96172f192459ac43ec335770620b
Jeppe Birkebæk <jexepp@gmail.com>
2018-04-25 15:19:40 +0200

    Case, methods

--- SenSafe Udred/src/sensafe/udred/business/Case.java
+++ SenSafe Udred/src/sensafe/udred/business/Case.java
@@ -4,19 +4,22 @@
  * and open the template in the editor.
  */
 package sensafe.udred.business;
+
 import java.util.ArrayList;
+
 /**
  *
  * @author jtrue
  */
 public class Case {
+
     private int CaseID;
     private String CaseResponsible;
     private String CitizenProfile;
     private String CaseDesc;
-    private String Relative; 
+    private String Relative;    
     private String Medicine;
-
+    
     public Case(int CaseID, String CaseResponsible, String CitizenProfile, String CaseDesc, String Relative, String Medicine) {
         this.CaseID = CaseID;
         this.CaseResponsible = CaseResponsible;
@@ -25,30 +28,28 @@ public class Case {
         this.Relative = Relative;
         this.Medicine = Medicine;
     }
-    
+
     //CaseDB
     ArrayList<Case> CaseList;
-    public void CaseDatabase(){
+    
+    public void CaseDatabase() {
         CaseList = new ArrayList<Case>();
     }
     
-    
-    
-    
-    public void caseCreate(int CaseID, String CaseResponsible, String CitizenProfile, String CaseDesc, String Relative, String Medicine){
-    CaseList.add(this);
+    public void caseCreate(int CaseID, String CaseResponsible, String CitizenProfile, String CaseDesc, String Relative, String Medicine) {
+        CaseList.add(this);
     }
     
-    public void caseDelete(int CaseID){
-        
+    public void caseDelete(int CaseID) {
+        CaseList.remove(CaseID);
     }
-
+    
     public int getCaseID() {
         return this.CaseID;
     }
     
-    public void caseSave() {
-        
+    public void caseSave(int CaseID) {
+        CaseList.add(CaseID, this);
     }
     
 }
