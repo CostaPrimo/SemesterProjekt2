7e38569fc8542ecc25f30ac030b07f9a8d3ecbe0
Zwips <32239749+Zwips@users.noreply.github.com>
2018-04-27 12:40:06 +0200

    mainframe
    Mainframe lavet med det der fungere lige nu.

--- SenSafe Udred/src/sensafe/udred/business/SenSafeUdred.java
+++ SenSafe Udred/src/sensafe/udred/business/SenSafeUdred.java
@@ -6,6 +6,7 @@
 package sensafe.udred.business;
 
 import java.io.File;
+import java.util.Scanner;
 
 /**
  *
@@ -17,13 +18,68 @@ public class SenSafeUdred {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         File file = new File("Test");
         UserDatabase database = new UserDatabase("test", "userDatabase.txt");
-        database.userCreate("Kim", "1923918");
-        database.userCreate("Trox", "hamme", 5000, "kim@kim.dk", "Handkkap", "2323232");
-        database.userChangeDepartment(1002, "BARBIE");
-        database.removeUserInfoFromFile(1001);
+        Scanner scanner = new Scanner(System.in);
+        boolean boo = true;
         
+        
+        
+        System.out.println("Tast 1 for at oprette en ny bruger");
+        System.out.println("Tast 2 for at slette en bruger");
+        System.out.println("Tast 3 for at se allerede oprettet brugere");
+        System.out.println("Tast 4 for at oprette en ny sag");
+        System.out.println("Tast 5 for at afslutte programmet");
+        
+        while(boo == true){
+            int input = scanner.nextInt();
+            
+            switch(input){
+                case 1: 
+                    System.out.println("indtast brugernavn");
+                    String username = scanner.next();
+                    System.out.println("Indtast navn");
+                    String name = scanner.next();
+                    System.out.println("Intast postnummer");
+                    int zipcode = scanner.nextInt();
+                    System.out.println("Intast email");
+                    String email = scanner.next();
+                    System.out.println("Indtast afdeling");
+                    String department = scanner.next();
+                    System.out.println("Intast telefonnummer");
+                    String phoneNumber = scanner.next();
+//                    System.out.println(username + name + zipcode + email + department + phoneNumber);
+                    database.userCreate(username, name, zipcode, email, department, phoneNumber);
+                    break;
+                case 2: 
+                    System.out.println("Indtast userID");
+                    int userID = scanner.nextInt();
+                    database.userDelete(userID);
+                    break;
+                case 3: 
+                    System.out.println("Ikke indsat endnu");
+                    break;
+                case 4: 
+                    System.out.println("Ikke indsat endnu");
+                    break;
+                case 5: 
+                    System.out.println("Ikke indsat endnu");
+                    boo = false;
+                default:
+                    System.out.println("Invalid input");
+               
+                
+            }
+        }
+//        
+//        File file = new File("Test");
+//        UserDatabase database = new UserDatabase("test", "userDatabase.txt");
+//        database.userCreate("Kim", "1923918");
+//        database.userCreate("Trox", "hamme", 5000, "kim@kim.dk", "Handkkap", "2323232");
+//        database.userChangeDepartment(1002, "BARBIE");
+//        database.removeUserInfoFromFile(1001);
+//        
     }
         
         
